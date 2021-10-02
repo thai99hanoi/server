d6807196bc136e6ca4334bc0f1f8469ef99e6327
Nguyễn Thị Lan Anh <lananh4415@gmail.com>
2021-10-03 03:31:10 +0700

    anhntl-socket

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -23,7 +23,7 @@ function connect(event) {
         usernamePage.classList.add('hidden');
         chatPage.classList.remove('hidden');
 
-        var socket = new SockJS('/test');
+        var socket = new SockJS('/chat-test');
         stompClient = Stomp.over(socket);
 
         stompClient.connect({}, onConnected, onError);
